5aafd69c8f6c0609b1855148aa9e8d396e1e7b83
Alexander Spanos <alexisspa9@gmail.com>
2018-03-29 08:54:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+
 class Student
   attr_accessor :id, :name, :grade
 
@@ -8,7 +9,6 @@ class Student
     student.name = row[1]
     student.grade = row[2]
     student
-
   end
 
   def self.all
@@ -28,8 +28,12 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
-    SELECT * FROM students WHERE name = ?
-    LIMIT 1 SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
     DB[:conn].execute(sql,name).map do |row|
       self.new_from_db(row)
     end.first
@@ -58,16 +62,31 @@ class Student
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
+
     DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-    SELECT * FROM students WHERE grade = 9
+      SELECT COUNT(*)
+      FROM students
+      WHERE grade = 9;
     SQL
+
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-
   end
 end
